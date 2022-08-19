0f9267d79b7103867bcde8c245913ae3fd4cdc63
ryota-sb <ryota.sb.1206@gmail.com>
2022-08-19 18:52:42 +0900

    blog 一覧ページ修正

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,17 @@
-import type { NextPage, InferGetStaticPropsType } from "next";
-import type { Blog } from "types/blog";
+import { NextPage, InferGetStaticPropsType } from "next";
 import Link from "next/link";
+import { useState } from "react";
+import { Blog } from "types/blog";
 import { client } from "libs/client";
-import { brotliDecompress } from "zlib";
+
+import { NavbarComponent } from "../components/NavbarComponent";
+import { FooterComponent } from "../components/FooterComponent";
+
+// Mantine UI
+import { AppShell, Header, Burger, Title } from "@mantine/core";
 
 export const getStaticProps = async () => {
   const blog = await client.get({ endpoint: "blog" });
-
   return {
     props: {
       blogs: blog.contents,
@@ -21,18 +26,38 @@ type Props = {
 const Home: NextPage<InferGetStaticPropsType<typeof getStaticProps>> = ({
   blogs,
 }: Props) => {
+  const [opened, setOpened] = useState<boolean>(false);
+
   return (
-    <div>
-      <ul>
-        {blogs.map((blog) => (
-          <li key={blog.id}>
-            <Link href={`/blog/${blog.id}`}>
-              <a>{blog.title}</a>
-            </Link>
-          </li>
-        ))}
-      </ul>
-    </div>
+    <AppShell
+      header={
+        <Header height={60}>
+          <div className="flex align-middle h-full">
+            <Burger
+              opened={opened}
+              onClick={() => setOpened((open) => !open)}
+              size="sm"
+              mr="xl"
+            />
+            <Title>Run Dev</Title>
+          </div>
+        </Header>
+      }
+      navbar={<NavbarComponent opened={opened} />}
+      footer={<FooterComponent />}
+    >
+      <main className="flex flex-1 flex-col justify-center p-4">
+        <ul>
+          {blogs.map((blog) => (
+            <li key={blog.id}>
+              <Link href={`/blog/${blog.id}`}>
+                <a>{blog.title}</a>
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </main>
+    </AppShell>
   );
 };
 
