95102131011dfe3fa12ef2789625b7937f62c1dd
ryota-sb <ryota.sb.1206@gmail.com>
2022-09-10 21:06:57 +0900

    grid layout 完成

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -27,21 +27,25 @@ const Home: NextPage<InferGetStaticPropsType<typeof getStaticProps>> = ({
 }: Props) => {
   console.log(blogs);
   return (
-    <div className="container mx-auto w-full">
-      <h1 className="px-10 font-raleway text-5xl">Ryota Code .</h1>
-      <div className="container grid w-full grid-cols-3 items-center gap-10 px-10">
+    <div className="container mx-auto w-full max-w-7xl px-10">
+      <h1 className="px-10 py-10 font-raleway text-4xl lg:text-5xl">
+        Ryota Code .
+      </h1>
+      <div className="grid w-full grid-cols-1 items-center gap-10 md:grid-cols-2 lg:grid-cols-2">
         {blogs.map((blog) => (
           <Link href={`/blog/${blog.id}`} key={blog.id}>
             <div className="cursor-pointer rounded-lg px-6 shadow-md hover:scale-110 hover:duration-300">
               <Image
                 src={blog.image.url}
                 alt={blog.title}
-                width={350}
-                height={150}
+                width={500}
+                height={250}
                 objectFit="contain"
               />
-              <h3 className="truncate text-center">{blog.title}</h3>
-              <p className="flex justify-end">
+              <h3 className="truncate text-center font-notoserif text-2xl">
+                {blog.title}
+              </h3>
+              <p className="flex justify-end py-4">
                 {getFormattedDate(blog.createdAt)}
               </p>
             </div>
