c0bd33d3e6ced340f9fd2639a5e69c62a8e471fb
ryota-sb <ryota.sb.1206@gmail.com>
2022-09-12 16:11:44 +0900

    アスペクト比 設定するためのTailwindCSS ライブラリ追加

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,5 +13,8 @@ module.exports = {
       },
     },
   },
-  plugins: [require("@tailwindcss/typography")],
+  plugins: [
+    require("@tailwindcss/typography"),
+    require("@tailwindcss/aspect-ratio"),
+  ],
 };
