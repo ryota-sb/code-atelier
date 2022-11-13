733b07c9ac9d9851f5af1c8a0844f7cdeb57a9b7
ryota-sb <ryota.sb.1206@gmail.com>
2022-11-13 19:31:25 +0900

    メインページのレイアウト修正

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,6 +9,7 @@ import { Blog } from "types/blog";
 import { client } from "libs/client";
 
 import Footer from "components/Footer";
+import Header from "components/Header";
 
 type Props = {
   blogs: Blog[];
@@ -32,14 +33,37 @@ const Home: NextPage<InferGetStaticPropsType<typeof getStaticProps>> = ({
 }: Props) => {
   return (
     <div className="flex h-screen flex-col bg-blue-100">
-      <div className="mx-auto  max-w-7xl flex-grow px-6 sm:px-8 lg:max-w-7xl lg:px-8">
-        <h1 className="px-10 py-16 font-raleway text-4xl lg:text-5xl">
-          Ryota Code .
-        </h1>
-        <div className="grid grid-cols-1 gap-y-8 gap-x-8 sm:grid-cols-2">
-          {blogs.map((blog) => (
-            <BlurImage key={blog.id} blog={blog} />
-          ))}
+      <Header />
+      <div className="grid flex-grow grid-cols-6 p-14">
+        {/* 記事一覧 */}
+        <div className="col-span-5 bg-white">
+          <div className="m-8 grid grid-cols-1 gap-x-8 sm:grid-cols-2 lg:grid-cols-3">
+            {blogs.map((blog) => (
+              <BlurImage key={blog.id} blog={blog} />
+            ))}
+          </div>
+        </div>
+
+        <div className="col-span-1 ml-14">
+          {/* About Me */}
+          <div className="mb-14 bg-white p-4">
+            <div className="grid grid-cols-6">
+              <div className="aspect-w-1 aspect-h-1 col-span-3">
+                <Image
+                  src="/imgs/ryota.jpg"
+                  layout="fill"
+                  objectFit="contain"
+                  className=""
+                />
+              </div>
+              <div className="col-span-3">
+                <h1>Matsui Ryota</h1>
+              </div>
+            </div>
+          </div>
+
+          {/* タグ一覧 */}
+          <div className="bg-white">Tags Field</div>
         </div>
       </div>
       <Footer />
@@ -53,7 +77,7 @@ const BlurImage = ({ blog }: { blog: Blog }) => {
   return (
     <Link href={`/blog/${blog.id}`}>
       <div className="group cursor-pointer">
-        <div className="aspect-w-16 aspect-h-9 w-full overflow-hidden rounded bg-gray-200">
+        <div className="aspect-w-16 aspect-h-9 w-full overflow-hidden border border-gray-200 bg-gray-200">
           <Image
             src={blog.image.url}
             alt=""
@@ -71,7 +95,7 @@ const BlurImage = ({ blog }: { blog: Blog }) => {
         <h3 className="mt-4 font-notoserif text-2xl text-gray-900">
           {blog.title}
         </h3>
-        <p className="mt-1 font-notoserif text-lg font-medium text-gray-900">
+        <p className="mt-1 font-notoserif text-sm font-medium text-gray-900">
           {getFormattedDate(blog.createdAt)}
         </p>
       </div>
