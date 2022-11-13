360f0d450f846e0ca2e718aca986df3a99290dcb
ryota-sb <ryota.sb.1206@gmail.com>
2022-11-13 19:30:09 +0900

    header component に切り分け

---
+++ src/components/Header.tsx
@@ -0,0 +1,15 @@
+import { FC } from "react";
+
+const Header: FC = () => {
+  return (
+    <header>
+      <div className="bg-white">
+        <h1 className="px-14 py-4 font-raleway text-2xl lg:text-3xl">
+          Code Atelier
+        </h1>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
