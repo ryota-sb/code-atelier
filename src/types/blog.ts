530ab389ab26816c130969b29ab6ca090e9bda05
ryota-sb <ryota.sb.1206@gmail.com>
2022-11-16 20:25:40 +0900

    タグのtype定義

--- src/types/blog.ts
+++ src/types/blog.ts
@@ -2,7 +2,17 @@ export type Blog = {
   id: string;
   body: string;
   title: string;
+  tags: Tag[];
   image: { url: string };
+  createdAt: Date;
+  updatedAt: Date;
+  publishedAt: Date;
+  revisedAt: string;
+};
+
+export type Tag = {
+  id: string;
+  tag: string;
   createdAt: Date;
   updatedAt: Date;
   publishedAt: Date;
