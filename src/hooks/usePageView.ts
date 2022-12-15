5fc4a66892749dab0b2652d6c297a4c7d1e21847
ryota-sb <ryota.sb.1206@gmail.com>
2022-12-15 20:06:49 +0900

    ページ遷移時に、PVカウントするためのhooksを追加、_appで呼び出し

---
+++ src/hooks/usePageView.ts
@@ -0,0 +1,20 @@
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+
+import { GA_ID, pageview } from "libs/gtag";
+
+export default function usePageView() {
+  const router = useRouter();
+  useEffect(() => {
+    if (!GA_ID) return;
+
+    const handleRouteChange = (url: string, { shallow }: any) => {
+      if (!shallow) pageview(url);
+    };
+
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
+}
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,11 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import usePageView from "hooks/usePageView";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  // Google Analytics の PageView をカウントする
+  usePageView();
+
   return <Component {...pageProps} />;
 }
 
