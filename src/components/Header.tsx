9570d8924a11624b971b65b718d53930d497b07b
ryota-sb <ryota.sb.1206@gmail.com>
2022-12-16 05:07:21 +0900

    ヘッダーのタイトルを押すと、ルートページに遷移するようにLink追加

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,11 @@
+import Link from "next/link";
+
 export default function Header() {
   return (
     <header>
       <div className="bg-white drop-shadow-md">
         <h1 className="px-14 py-4 font-raleway text-2xl lg:text-3xl">
-          Code Atelier .
+          <Link href="/">Code Atelier .</Link>
         </h1>
       </div>
     </header>
