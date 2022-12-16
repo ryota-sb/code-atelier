d51841e4374b7c1bc7e984a56812fd35c83c255c
ryota-sb <ryota.sb.1206@gmail.com>
2022-12-16 21:03:06 +0900

    SideMenuコンポーネント追加

---
+++ src/components/SideMenu.tsx
@@ -0,0 +1,32 @@
+import AboutMe from "components/AboutMe";
+import Tags from "components/Tags";
+
+import getWindowSize from "hooks/getWindowSize";
+
+import type { Tag } from "types/blog";
+
+type Props = {
+  tags: Tag[];
+};
+
+// 画面サイズがxl以上ならtrueを返す
+function isBreakPointXl() {
+  const { height, width } = getWindowSize();
+  const BREAK_POINT_PX_XL = 1280;
+  return width >= BREAK_POINT_PX_XL;
+}
+
+export default function SideBar({ tags }: Props) {
+  return (
+    <div>
+      {isBreakPointXl() ? (
+        <div className="ml-10 lg:col-span-1">
+          {/* 自己紹介 */}
+          <AboutMe />
+          {/* タグ一覧 */}
+          <Tags tags={tags} />
+        </div>
+      ) : null}
+    </div>
+  );
+}
