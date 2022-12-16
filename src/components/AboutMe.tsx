b598a7dd5cd00fdbc10efe4ca0e6a2c077c3b039
ryota-sb <ryota.sb.1206@gmail.com>
2022-12-16 21:01:43 +0900

    Aboutをコンポーネントに切り分け

---
+++ src/components/AboutMe.tsx
@@ -0,0 +1,22 @@
+import Image from "next/legacy/image";
+
+export default function AboutMe() {
+  return (
+    <div className="mb-10 bg-white p-6">
+      <div className="grid grid-cols-2 gap-x-6">
+        <div className="aspect-w-1 aspect-h-1 col-span-1">
+          <Image
+            src="/imgs/ryota.jpg"
+            layout="fill"
+            objectFit="contain"
+            className="rounded-md"
+            alt="Ryota"
+          />
+        </div>
+        <div className="col-span-1">
+          <h1 className="font-raleway">Matsui Ryota</h1>
+        </div>
+      </div>
+    </div>
+  );
+}
