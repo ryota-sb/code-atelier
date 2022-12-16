59c63e2a55141c1c3af9479e4d061ca3b04bf5e1
ryota-sb <ryota.sb.1206@gmail.com>
2022-12-16 21:03:44 +0900

    画面サイズを取得するhooksを定義

---
+++ src/hooks/getWindowSize.ts
@@ -0,0 +1,22 @@
+import { useEffect, useState } from "react";
+
+export default function getWindowSize() {
+  const [windowSize, setWindowSize] = useState({ width: 0, height: 0 });
+
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      const handleResize = () => {
+        setWindowSize({
+          width: window.innerWidth,
+          height: window.innerHeight,
+        });
+      };
+      window.addEventListener("resize", handleResize);
+      handleResize();
+      return () => window.removeEventListener("resize", handleResize);
+    } else {
+      return;
+    }
+  }, []);
+  return windowSize;
+}
