5969edebc261ead4be5675cc2027da214f1e2df2
ryota-sb <ryota.sb.1206@gmail.com>
2023-10-19 20:57:56 +0900

    microCMSのデータ取得処理をclientファイルに集約

--- app/libs/client.ts
+++ app/libs/client.ts
@@ -2,6 +2,11 @@ import { createClient } from "microcms-js-sdk";
 
 import type { Blog, Tag } from "types/blog";
 
+// 記事のコードエリアのシンタックスハイライト
+import cheerio from "cheerio";
+import hljs from "highlight.js";
+import "highlight.js/styles/hybrid.css";
+
 export const client = createClient({
   serviceDomain: process.env.SERVICE_DOMAIN || "",
   apiKey: process.env.API_KEY || "",
@@ -21,4 +26,54 @@ export const getTags = async () => {
     endpoint: "tag",
   });
   return tagData.contents as Tag[];
+};
+
+// Blogのパスを配列で取得
+export const getAllBlogIds = async () => {
+  const blogs = await getBlogs();
+  const paths = blogs.map((blog: Blog) => {
+    id: `/blog/${blog.id}`;
+  });
+  return paths;
+};
+
+// Tagのパスを配列で取得
+export const getAllTagIds = async () => {
+  const tags = await getTags();
+  const paths = tags.map((tag: Tag) => {
+    id: `/tagsBlog/${tag.id}`;
+  });
+  return paths;
+};
+
+export const getBlog = async (blogId: string) => {
+  const blogData = await client.get({
+    endpoint: "blog",
+    contentId: blogId,
+  });
+
+  const $ = cheerio.load(blogData.body || "");
+  $("pre code").each((_, elm) => {
+    const result = hljs.highlightAuto($(elm).text());
+    $(elm).html(result.value);
+    $(elm).addClass("hljs");
+  });
+
+  return {
+    blog: blogData,
+    highlightedBody: $.html(),
+  };
+};
+
+// TagIdで絞り込んだBlog記事取得
+export const filterBlogsByTag = async (tagId: string) => {
+  const filterBlogData = await client.get({
+    endpoint: "blog",
+    queries: { filters: `tags[contains]${tagId}` },
+  });
+  const tags = await getTags();
+  return {
+    blogs: filterBlogData.contents as Blog[],
+    tags: tags,
+  };
 };
