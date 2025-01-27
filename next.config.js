92f54aea602ebc1322ac314f28c92b203c21e79e
ryota-sb <ryota.sb.1206@gmail.com>
2025-01-28 00:44:07 +0900

    fix: experimentalのappDirの設定を削除

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-  experimental: {
-    appDir: true
-  },
   trailingSlash: true,
   images: {
     domains: ["images.microcms-assets.io"],
